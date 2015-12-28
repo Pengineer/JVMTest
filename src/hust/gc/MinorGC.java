ef689c2e720d274d453c8ec5073209b1b66df315
pengliang <990607342@qq.com>
2015-12-28 19:10:34 +0800

    perfect MinorGC

--- src/hust/gc/MinorGC.java
+++ src/hust/gc/MinorGC.java
@@ -1,7 +1,7 @@
 package hust.gc;
 
 /**
- * 
+ * 测试新生代Minor GC
  * 
  * @author 2015-12-25
  *
@@ -37,8 +37,12 @@ Heap
 No shared spaces configured.
 
 解释：
-6472K->140K(9216K):因为分配allocation4时新生代空间不足，触发GC，新生代占用量减少。
-6472K->6284K(19456K)：总堆占用量不变。因为allocation1~3对象依然存活，因此新生代剩余空间不足以分配给4，导致Minor GC的触发，allocation1~3通过分配担保机制提前转移到老年代。
-def new generation   total 9216K：可能会有疑惑，新生代不是10M吗，怎么成了9M。因为新生代采用的复制算法回收空间，有一个Survivor（1M）被用来作为回收对象使用，因此这里显示的可用新生代总空间。
-tenured generation   total 10240K：老年代6144/10240。
+（1）6472K->140K(9216K):因为分配allocation4时新生代空间不足，触发GC，新生代占用量减少。
+（2）6472K->6284K(19456K)：总堆占用量不变。因为allocation1~3对象依然存活，因此新生代剩余空间不足以分配给4，导致
+	 Minor GC的触发，allocation1~3通过分配担保机制提前转移到老年代。（为什么说提前呢，因为在分代收集思想中，虚拟机给
+	 每一个对象定义了对象年龄计数器，对象每经历一次Minor GC且存活，计数器加1，直到-XX:MaxTenuringThreshold参数设置的
+	 阀值，然后转为老年代，当然这是一般情况。）
+（3）def new generation total 9216K：可能会有疑惑，新生代不是10M吗，怎么成了9M。因为新生代采用的复制算法回收空间，
+	有一个Survivor（1M）作为轮换备份来保存回收的存活对象，因此这里显示的可用新生代总空间。
+（4）tenured generation total 10240K：老年代6144/10240。
 */
